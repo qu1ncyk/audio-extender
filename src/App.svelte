bb4b719a5a43e17b02798e18b8b0cfb7b7c84205
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2021-07-21 12:14:36 +0000

    created the ui for the file picker

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,22 @@
 <script lang="ts">
-	export let name: string;
+	import FilePicker from "./FilePicker.svelte";
 </script>
 
+<svelte:head>
+	<title>Audio Extender</title>
+</svelte:head>
+
+<header>
+	<h1>Audio Extender</h1>
+</header>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<FilePicker />
 </main>
 
 <style>
-	main {
+	main,
+	header {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 </style>
