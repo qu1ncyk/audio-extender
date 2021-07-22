0e8fd82cb40504b4790629f2eaef131723cd5160
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2021-07-22 12:22:16 +0000

    created the time input

--- src/Extender.svelte
+++ src/Extender.svelte
@@ -1,5 +1,33 @@
 <script lang="ts">
     import Player from "./Player.svelte";
+    import TimeInput from "./TimeInput.svelte";
 </script>
 
 <Player />
+
+<div class="input-container">
+    <div class="time-input">
+        <h2>Loop from</h2>
+        <TimeInput />
+    </div>
+    <div class="time-input">
+        <h2>until</h2>
+        <TimeInput />
+    </div>
+</div>
+
+<style>
+    .input-container {
+        display: flex;
+        justify-content: center;
+        gap: 0.2em;
+        margin-top: 0.5em;
+        flex-wrap: wrap;
+    }
+
+    .time-input {
+        border: 1px solid #ddd;
+        padding: 0.2em;
+        background-color: #eee;
+    }
+</style>
---
+++ src/TimeInput.svelte
@@ -0,0 +1,55 @@
+<div class="input-container">
+    <button class="row1 col1">-1ms</button>
+    <button class="row1 col2">-10ms</button>
+    <button class="row2 col1">-0.1s</button>
+    <button class="row2 col2">-1s</button>
+
+    <input type="text" />
+
+    <button class="row1 col3">+1ms</button>
+    <button class="row1 col4">+10ms</button>
+    <button class="row2 col3">+0.1s</button>
+    <button class="row2 col4">+1s</button>
+</div>
+
+<style>
+    .input-container {
+        display: grid;
+        grid-template-columns: repeat(2, [button] 1fr) [input] minmax(4em, 6em) repeat(2, [button] 1fr);
+        grid-template-rows: repeat(2, 1fr);
+        gap: 0.2em;
+    }
+
+    input {
+        width: 100%;
+        margin: 0;
+        grid-column: input;
+        grid-row: 1 / span 2;
+        place-self: center;
+    }
+
+    button {
+        margin: 0;
+        padding: 0.3em;
+    }
+
+    .row1 {
+        grid-row: 1;
+    }
+    .row2 {
+        grid-row: 2;
+    }
+
+    .col1 {
+        grid-column: button 1;
+    }
+    .col2 {
+        grid-column: button 2;
+    }
+    .col3 {
+        grid-column: button 3;
+    }
+    .col4 {
+        grid-column: button 4;
+    }
+</style>
