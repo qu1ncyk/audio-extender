512ba26bad81b8a7a53e81a760eaa7bb06aecc44
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2022-04-19 15:18:29 +0000

    changed the UI of the player

--- src/Player.svelte
+++ src/Player.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
-    import Fa from "svelte-fa";
-    import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
-    import { file, duration, loopStart, loopEnd, audioBuffer } from "./stores";
+    import {
+        file,
+        duration,
+        loopStart,
+        loopEnd,
+        audioBuffer,
+        storedInLibrary,
+    } from "./stores";
     import { secondsToTime } from "./convert-time";
-    import { createEventDispatcher } from "svelte";
+
+    import Card from "@smui/card";
+    import FormField from "@smui/form-field";
+    import CircularProgress from "@smui/circular-progress";
+    import IconButton from "@smui/icon-button";
+    import SvgIcon from "./SvgIcon.svelte";
+    import { mdiPlay, mdiPause } from "@mdi/js";
+    import Slider from "@smui/slider";
+    import Snackbar, {Label} from "@smui/snackbar";
+    import type {SnackbarComponentDev} from "@smui/snackbar";
 
     let audioContext = new AudioContext();
     let audioSource: AudioBufferSourceNode;
@@ -65,11 +79,13 @@
         }
         if (!sliding) sliderValue = Date.now() / 1000 - startingTime;
 
-        if (playing) requestAnimationFrame(progressLoop);
+        // the slider hangs with requestAnimationFrame
+        if (playing) setTimeout(progressLoop, 20);
     }
 
-    let dispatch = createEventDispatcher();
+    let snackbar: SnackbarComponentDev;
 
+    let loading = true;
     (async () => {
         try {
             let clonedFile = new ArrayBuffer($file.byteLength);
@@ -79,98 +95,57 @@
             $audioBuffer = await audioContext.decodeAudioData(clonedFile);
             audioSource.buffer = $audioBuffer;
             $duration = audioSource.buffer.duration;
-            dispatch("duration");
+
+            if (!$storedInLibrary) $loopEnd = $duration;
+
+            loading = false;
         } catch (e) {
             console.error(e);
-            alert("Could not play the audio file");
+            snackbar.open();
         }
     })();
 </script>
 
-<div class="player-container">
-    <span class="play-button" on:click={togglePlaying}>
-        {#if playing}
-            <Fa icon={faPause} />
+<Card style="padding: 4px;">
+    <FormField>
+        {#if loading}
+            <CircularProgress
+                indeterminate
+                style="width: 24px; height: 24px; margin: 12px;"
+            />
         {:else}
-            <Fa icon={faPlay} />
+            <IconButton on:click={togglePlaying}>
+                <SvgIcon icon={playing ? mdiPause : mdiPlay} />
+            </IconButton>
         {/if}
-    </span>
-    <span class="current time">{secondsToTime(Math.floor(sliderValue))}</span>
-    <input
-        type="range"
-        bind:value={sliderValue}
-        on:mousedown={slideStart}
-        on:mouseup={slideEnd}
-        on:touchstart={slideStart}
-        on:touchend={slideEnd}
-        max={$duration}
-        step="any"
-        style={`
-            --loop-start: ${($loopStart / $duration) * 100}%;
-            --loop-end: ${($loopEnd / $duration) * 100}%;
-        `}
-    />
-    <span class="end time">{secondsToTime(Math.floor($duration))}</span>
-</div>
+
+        <span class="current time">
+            {secondsToTime(Math.floor(sliderValue))}
+        </span>
+
+        <Slider
+            max={Math.max($duration, 1)}
+            bind:value={sliderValue}
+            step={0.001}
+            style="width: 100%;"
+            on:SMUISlider:input={slideStart}
+            on:SMUISlider:change={slideEnd}
+        />
+
+        <span class="end time">{secondsToTime(Math.floor($duration))}</span>
+    </FormField>
+</Card>
+
+<Snackbar bind:this={snackbar}>
+    <Label>Could not play the audio file</Label>
+</Snackbar>
 
 <style>
-    .player-container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-color: #eee;
-        border: 1px solid #ddd;
-        padding: 0.7em 0.5em;
-        margin: 0 auto;
-        width: 50em;
-        max-width: 90%;
-    }
-
-    .play-button {
-        cursor: pointer;
-    }
-
     .current.time {
         width: 4em;
     }
 
     .end.time {
-        margin-left: 0.5em;
-    }
-
-    input[type="range"] {
-        -webkit-appearance: none;
-        appearance: none;
-        height: 1.3em;
-        padding: 0;
-        background: linear-gradient(
-            to right,
-            #d7d7d7 var(--loop-start),
-            #ccc var(--loop-start),
-            #ccc var(--loop-end),
-            #d7d7d7 var(--loop-end)
-        );
-        border-radius: 0.65em;
-        outline: none;
-        touch-action: pan-y;
-        margin: 0;
-        flex-grow: 1;
-    }
-
-    input[type="range"]::-webkit-slider-thumb {
-        -webkit-appearance: none;
-        appearance: none;
-        width: 1.5em;
-        height: 1.5em;
-        background: white;
-        cursor: pointer;
-        border-radius: 50%;
-    }
-    input[type="range"]::-moz-range-thumb {
-        width: 1.5em;
-        height: 1.5em;
-        background: white;
-        cursor: pointer;
-        border-radius: 50%;
+        margin: 0 0.5em;
     }
 </style>
